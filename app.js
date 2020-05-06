b56d0013662692ce0757ea7ffe27045d2d973335
JrzenonDev <jrobertoonb@gmail.com>
2020-05-06 00:00:21 -0300

    chore: configurando a template engine(handlebars) e fazendo a conexão com o mysql no app

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 
+// Config
+// Template Engine
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// conexao
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sistemaDeCadatro', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 
 
