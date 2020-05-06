09761835ba1a0834965bcb737bee71f81e5f71fe
JrzenonDev <jrobertoonb@gmail.com>
2020-05-05 23:32:08 -0300

    chore: arquivo core da aplicação, rodando o servidor com express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+
+
+
+
+// start server
+app.listen(8081, function() {
+    console.log("Servidor rodando");
+});
+
